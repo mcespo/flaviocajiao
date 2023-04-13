cc74514ee206941b19e85bdb00985db602bfd63f
mcespo <mcespo@gmail.com>
2023-04-12 22:29:47 -0500

    sections

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <script setup>
+import Navigation from './components/Navigation.vue'
 import Header from './components/Header.vue'
+import Intro from './components/Intro.vue'
+import Contact from './components/Contact.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <main>
+    <Navigation />
     <Header />
+    <Intro />
+    <Contact />
+    <Footer />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
